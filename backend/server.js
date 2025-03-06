c1917241b889e485dafc13c3ca7e10fbc343c93b
Zeynep Yılmaz <zeynepgulcanyilmaz@gmail.com>
2025-03-06 12:08:50 +0300

    backend updatess

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');  // Axios'u import ettik
+const axios = require('axios');  
 const cors = require('cors');
 const app = express();
 const port = 5000;
@@ -18,20 +18,6 @@ app.get('/data', (req, res) => {
         location: 'TURKIYE',
     };
     res.json(data);
-});
-
-app.get('/health', (req, res) => {
-    axios.get('http://34.133.27.32:30922/health')  
-        .then(response => {
-            if (response.status === 404) {
-                res.status(404).send('Backend is healthy and external health check passed!');
-            } else {
-                res.status(500).send('External health check failed.');
-            }
-        })
-        .catch(error => {
-            res.status(500).send('Error with external health check.');
-        });
 });
 
 app.listen(port, () => {
